aa3cbd6fda6af9ee80b3135a3125760b13432355
juanjo-18 <juanjomedinadiaz@gmail.com>
2024-02-21 13:48:32 +0100

    up

--- src/app.py
+++ src/app.py
@@ -111,14 +111,14 @@ def pagina_categoria_1():
             
 
             # Imprimir la predicción de piel o piel cancer
-            st.write("La prediccion es piel cancer al : ",resultados[0][0])
-            st.write("La prediccion es piel sana al : ",resultados[0][1])
+            st.write("La prediccion es piel sana al : ",resultados[0][0])
+            st.write("La prediccion es piel cancer al : ",resultados[0][1])
 
-            clase_predicha = np.argmax(resultados)
-            if clase_predicha == 0:
-                st.write("La imagen es piel cancer.")
-            else:
+            clase_predicha = np.argmax(resultados[0][0])
+            if clase_predicha >= 0.75:
                 st.write("La imagen es piel sana.")
+            else:
+                st.write("La imagen es piel cancer.")
             
 
 
