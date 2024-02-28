8a401730db55b76f5719b2e2f2272520cdb4a3bb
JesusCanovas <146923591+JesusCanovas@users.noreply.github.com>
2024-02-28 22:35:56 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -213,9 +213,9 @@ def pagina_categoria_2():
                 comentario3 = df_ordenado.iloc[2]['COMENTARIO']
                 puntuacion3= round(df_ordenado.iloc[2]['PUNTUACION'],2)
                 
-                mejor_texto1 = f"1. {comentario1}  -  {puntuacion1}"
-                mejor_texto2 = f"2. {comentario2}  -  {puntuacion2}"
-                mejor_texto3 = f"3. {comentario3}  -  {puntuacion3}"
+                mejor_texto1 = f"1. '{comentario1}'  -  {puntuacion1}"
+                mejor_texto2 = f"2. '{comentario2}'  -  {puntuacion2}"
+                mejor_texto3 = f"3. '{comentario3}'  -  {puntuacion3}"
 
                 # Ordenar el DataFrame por puntuación en orden ascendente
                 df_ordenado = df.sort_values(by='PUNTUACION', ascending=True)
@@ -228,9 +228,9 @@ def pagina_categoria_2():
                 comentario3 = df_ordenado.iloc[2]['COMENTARIO']
                 puntuacion3= round(df_ordenado.iloc[2]['PUNTUACION'],2)
                 
-                peor_texto1 = f"1. {comentario1}  -  {puntuacion1}"
-                peor_texto2 = f"2. {comentario2}  -  {puntuacion2}"
-                peor_texto3 = f"3. {comentario3}  -  {puntuacion3}"
+                peor_texto1 = f"1. '{comentario1}'  -  {puntuacion1}"
+                peor_texto2 = f"2. '{comentario2}'  -  {puntuacion2}"
+                peor_texto3 = f"3. '{comentario3}'  -  {puntuacion3}"
 
                 # Crear un bloque vacío de 10px de altura usando HTML personalizado
                 bloque_vacio = '<div style="height: 20px;"></div>'
