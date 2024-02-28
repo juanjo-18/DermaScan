e578310351fda6a605693eb11ed2a72c36e760f4
JesusCanovas <146923591+JesusCanovas@users.noreply.github.com>
2024-02-28 12:35:30 +0100

    update app.py
    descripciones y fotografias de pagina 2

--- src/app.py
+++ src/app.py
@@ -299,21 +299,26 @@ def pagina_categoria_2():
     # Sección de Tiempo de Exposición
     with col4:
         st.subheader("Tiempo de Exposición al Sol")
-        st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ac nisi vel nunc feugiat suscipit. Integer non lorem metus.")
-        #st.image("ruta_imagen_tiempo_exposicion.jpg", use_column_width=True)
+        st.image("imagenes/sun_1.png", use_column_width=True)
+        st.write("Limita tu tiempo de exposición al sol, especialmente durante las horas pico de radiación solar.")
+        st.write("Usa ropa protectora, sombreros de ala ancha y busca sombra para reducir la exposición directa al sol.")
+        st.write("Aplica protector solar de amplio espectro con factor de protección solar (FPS) 30 o superior cada dos horas, o más frecuentemente si sudas o te mojas.")
 
     # Sección de Hidratación
     with col5:
         st.subheader("Hidratación")
-        st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ac nisi vel nunc feugiat suscipit. Integer non lorem metus.")
-        #st.image("ruta_imagen_hidratacion.jpg", use_column_width=True)
+        st.image("imagenes/water_1.png", use_column_width=True)
+        st.write("Mantente bien hidratado bebiendo suficiente agua a lo largo del día, especialmente en climas cálidos o cuando haces ejercicio.")
+        st.write("Considera el consumo de frutas y verduras con alto contenido de agua, como sandía, pepino y naranjas, para ayudar a mantener la hidratación.")
+        st.write("Evita el exceso de cafeína y alcohol, ya que pueden tener un efecto deshidratante en el cuerpo.")
 
     # Sección de Dieta Saludable
     with col6:
         st.subheader("Dieta Saludable")
-        st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ac nisi vel nunc feugiat suscipit. Integer non lorem metus.")
-        #st.image("ruta_imagen_dieta.jpg", use_column_width=True)
-
+        st.image("imagenes/food_1.png", use_column_width=True)
+        st.write("Prioriza una dieta rica en frutas, verduras, granos enteros y proteínas magras para obtener los nutrientes necesarios para una piel saludable.")
+        st.write("Los alimentos ricos en antioxidantes, como las bayas, el té verde y las nueces, pueden ayudar a proteger la piel del daño causado por los radicales libres.")
+        st.write("Evita los alimentos procesados, altos en azúcares añadidos y grasas saturadas, ya que pueden contribuir a problemas cutáneos como el acné y la inflamación.")
     
     # División de la pantalla en columnas proporcionales
     col7, col8, col9 = st.columns([1, 1, 1])
