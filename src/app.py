f666e6566607f1588f9da8cd2d28efd72d1bd9de
JesusCanovas <146923591+JesusCanovas@users.noreply.github.com>
2024-03-01 13:26:05 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -22,27 +22,11 @@ from prevencion import Prevencion
 from indice_uv import Indice_UV
 from about_us import About_US
 
-import streamlit.components.v1 as components
-from hydralit_components import IS_RELEASE
-
-PINNED_NAV_STYLE = """
-                    <style>
-                    .reportview-container .sidebar-content {
-                        padding-top: 0rem;
-                    }
-                    .reportview-container .main .block-container {
-                        padding-top: 1rem;
-                        padding-right: 1rem;
-                        padding-left: 1rem;
-                        padding-bottom: 0rem;
-                    }
-                    </style>
-                """
 
 if __name__ == '__main__':
 
     # ESTA ES LA PAGINA HOST A LA QUE LE AÑADIMOS LAS HIJAS
-    app = HydraApp(title='DermaScan',favicon="🔬")
+    app = HydraApp(title='DermaScan',favicon="🔬", hide_streamlit_markers=False, sticky_mode = 'pinned')
   
     # AÑADIMOS LAAS CLASES
     app.add_app("Inicio", icon="🔬", app=Home())
@@ -51,6 +35,6 @@ if __name__ == '__main__':
     app.add_app("Indice UV", icon="☀️", app=Indice_UV())
     app.add_app("Sobre nosotros", icon="👥", app=About_US())
    
-    st.markdown(PINNED_NAV_STYLE,unsafe_allow_html=True)
+
     # EJECUTA EL MAIN
     app.run()
