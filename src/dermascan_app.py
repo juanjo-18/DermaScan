720991c073a5913779ccb67f1d67262b7a9a2bf1
juanjo-18 <juanjomedinadiaz@gmail.com>
2024-03-05 22:56:57 +0100

    up

--- src/dermascan_app.py
+++ src/dermascan_app.py
@@ -140,7 +140,7 @@ class DermascanApp(HydraHeadApp):
                                     elif clase_predicha == 1:
                                         st.write("El tipo de cancer es Melanocytic nevus ")
                                     else:
-                                        st.write("El tipo de cancer es Queratosis seborreica")
+                                        
                                 else:
                                     st.write("La imagen insertada es un cancer de piel maligno.")
                                     # Imprimir la predicción de clasificacion maligno
