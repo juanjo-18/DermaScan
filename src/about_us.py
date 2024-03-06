b606fb3e29d7b9b4a888a5c01a9a0c38af7ff460
JesusCanovas <146923591+JesusCanovas@users.noreply.github.com>
2024-03-06 02:02:58 +0100

    Update about_us.py

--- src/about_us.py
+++ src/about_us.py
@@ -23,9 +23,9 @@ class About_US(HydraHeadApp):
 # PEGAMOS NUESTRO CODIGO DE PAGINA
     def run(self):
         
-        st.title("DermaScan")
+        
         st.markdown(f"<h1 style='text-align:center;'>Conoce a los Creadores de DermaScan</h1>", unsafe_allow_html=True)
-        st.markdown(f"<h2 style='text-align:center;'>¡Bienvenido a nuestra sección 'About Us'! Aquí puedes conocer más sobre los creadores detrás de DermaScan: Juanjo Medina y Jesús Cánovas.</h2>", unsafe_allow_html=True)
+        st.markdown(f"<h2 style='text-align:center;'>¡Bienvenido a nuestra sección 'Sobre nosotros'! Aquí puedes conocer más sobre los creadores detrás de DermaScan: Juanjo Medina y Jesús Cánovas.</h2>", unsafe_allow_html=True)
         st.markdown(f"<p style='text-align:center;'>Somos Juanjo Medina y Jesús Cánovas, dos apasionados del aprendizaje automático y la ciencia de datos. Nos conocimos mientras realizábamos un Máster en inteligencia artificial y Big Data en el centro integrado de formación profesional Alan Turing, y desde entonces hemos estado trabajando juntos en proyectos innovadores. La idea de desarrollar DermaScan surgió de nuestra pasión por la tecnología y nuestra conciencia sobre los desafíos en la detección temprana del cáncer de piel. Únete a nosotros en nuestra misión de concienciar y prevenir el cáncer de piel mientras exploramos los límites de la inteligencia artificial y la salud digital.</p>", unsafe_allow_html=True)
         st.markdown(f"<p style='text-align:center;'>Nuestra inspiración para desarrollar DermaScan surgió de la necesidad de crear un modelo de inteligencia artificial para la detección temprana de enfermedades de la piel. </p>", unsafe_allow_html=True)
         st.markdown(f"<p style='text-align:center;'>Como residentes de Málaga, somos conscientes de la importancia de proteger la piel contra la radiación solar UV, lo que nos impulsó a crear esta aplicación innovadora como nuestro proyecto final de máster. Únete a nosotros en nuestra misión sobre el concienciamiento y la precvención del cáncer de piel.</p>", unsafe_allow_html=True)
