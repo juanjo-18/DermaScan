c472be5a2d4f25c6807dc6494ac49e28df26d156
JesusCanovas <146923591+JesusCanovas@users.noreply.github.com>
2024-03-13 00:11:15 +0100

    instaling spacy

--- src/home.py
+++ src/home.py
@@ -23,7 +23,29 @@ class Home(HydraHeadApp):
 
 # PEGAMOS NUESTRO CODIGO DE PAGINA 
     def run(self):
+        
+        @st.cache(allow_output_mutation=True)
+        def get_base64_of_bin_file(bin_file):
+            with open(bin_file, 'rb') as f:
+                data = f.read()
+            return base64.b64encode(data).decode()
 
+        def set_png_as_page_bg(png_file):
+            bin_str = get_base64_of_bin_file(png_file)
+            page_bg_img = '''
+            <style>
+            body {
+            background-image: url("data:image/png;base64,%s");
+            background-size: cover;
+            }
+            </style>
+            ''' % bin_str
+            
+            st.markdown(page_bg_img, unsafe_allow_html=True)
+            return
+
+        set_png_as_page_bg('background.png')
+        
         with st.container():
             style_image1 = """
             display: block;
