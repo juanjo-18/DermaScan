f63ab062f203317b823547777625e465ec1b1eee
JesusCanovas <146923591+JesusCanovas@users.noreply.github.com>
2024-03-13 09:03:37 +0100

    Update prevencion.py

--- src/prevencion.py
+++ src/prevencion.py
@@ -149,7 +149,7 @@ class Prevencion(HydraHeadApp):
             justify-content: center;
             border-radius: 20%;
             """
-            st.markdown(f'<img src="./app/static/derma5_prev.jpg" height="600" style="{style_image5}">', unsafe_allow_html=True)
+            st.markdown(f'<img src="./app/static/d5_prev.jpg" height="600" style="{style_image5}">', unsafe_allow_html=True)
             
 
         with col13:
